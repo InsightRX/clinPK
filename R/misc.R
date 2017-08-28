1e63c98d2240e8bedec4eaa6712ecb97b00ff562
ronkeizer <ronkeizer@gmail.com>
2017-08-28 13:19:45 -0700

    update is.nil function

--- R/misc.R
+++ R/misc.R
@@ -2,7 +2,7 @@
 #'
 #' @param x vector
 is.nil <- function(x = NULL) {
-  return(is.null(x) || any(c(length(x) == 0, is.na(x), x == "")))
+  return(is.null(x) || any(c(length(x) == 0, is.na(x), is.nan(x), x == "")))
 }
 
 #' factors or characters to numeric
