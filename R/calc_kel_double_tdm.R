7026287d758d79e83ee4eec62329738fc59c0010
ronkeizer <ronkeizer@gmail.com>
2017-12-13 13:25:32 -0800

    small fixes variable / argument names

--- R/calc_kel_double_tdm.R
+++ R/calc_kel_double_tdm.R
@@ -1,12 +1,11 @@
 #' Calculate elimination rate when given two TDM samples
 #'
 #' @param dose dose amount
-#' @param V volume of distribution
 #' @param t time or time after dose, vector of size 2
 #' @param dv observed value, vector of size 2
 #' @param tau dosing interval
 #' @param t_inf infusion time
-#' @param steady_state assume taken at steady state?
+#' @param steady_state samples taken at steady state?
 #' @param trapezoidal use trapezoidal rule to calculate AUC? Otherwise use dose/CL.
 #' @param return_parameters return all parameters instead of only kel?
 #' @examples
@@ -14,12 +13,10 @@
 #' @export
 calc_kel_double_tdm <- function (
   dose = 1000,
-  V = 50,
   t = c(2, 11.5),
   dv = c(30, 10),
   tau = 12,
   t_inf = 1,
-  auc24 = FALSE,
   steady_state = TRUE,
   trapezoidal = FALSE,
   return_parameters = FALSE
@@ -31,11 +28,11 @@ calc_kel_double_tdm <- function (
   t12 <- log(2) / kel
   cmax <- max(dv) / exp(-kel * (t[1] - t_inf))
   cmin <- cmax * exp(-kel * (tau - t_inf))
-  Vd <- (dose/t_inf) * (1-exp(-kel * t_inf)) / (kel*cmax)
+  V <- (dose/t_inf) * (1-exp(-kel * t_inf)) / (kel*cmax)
   if(steady_state) {
-    Vd <- Vd * 1/(1-exp(-kel * tau))
+    V <- V * 1/(1-exp(-kel * tau))
   }
-  CL <- Vd * kel
+  CL <- V * kel
   if(trapezoidal) {
     AUC <- ((cmax - cmin) / (log(cmax) - log(cmin))) * (tau - t_inf)
     if(steady_state) {
@@ -46,16 +43,16 @@ calc_kel_double_tdm <- function (
   } else {
     AUC <- dose / CL
   }
-  if(auc24) AUC <- AUC * (24/tau)
   if(return_parameters) {
     return(list(
       kel = kel,
       t12 = t12,
-      Vd = Vd,
+      V = V,
       CL = CL,
-      AUC = AUC,
       cmax = cmax,
-      cmin = cmin
+      cmin = cmin,
+      AUCss = AUC,
+      AUCss24 = AUC * (24/tau)
     ))
   }
   return(kel)
