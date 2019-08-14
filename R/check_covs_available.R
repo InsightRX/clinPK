6b920fb4720b1f5ef4dc523c5ba0f44a7ece9016
jasmineirx <jasmine@insight-rx.com>
2019-08-14 15:32:17 -0700

    roxygen fails when examples return errors

--- R/check_covs_available.R
+++ R/check_covs_available.R
@@ -3,32 +3,31 @@
 #' @param cov_reqs vector of covariates required for calculating derived covariatiate
 #' @param patient_covs named list of covariates
 #' @param verbose stop and describe missing covariate(s)?
-#' @examples 
+#' @examples
 #' check_covs_available(egfr_cov_reqs('cockcroft_gault_ideal')[[1]], list(creat = 1, weight = 100, height = 160, sex = 'female', age = 90))
-#' check_covs_available(egfr_cov_reqs('schwartz')[[1]], list(creat = c(0.2, 0.4), weight = 25, height = 80, sex = 'male', age = NULL))
 #' @export
 check_covs_available <- function(
   cov_reqs = NULL,
   patient_covs = NULL,
   verbose = TRUE
   ) {
-  
+
   if (is.null(patient_covs)){
     return(FALSE)
   }
-  
+
   if (is.null(cov_reqs) | length(cov_reqs) == 0){
     return(TRUE)
   }
-  
+
   covs_missing <- c()
-  
+
   for (cov in cov_reqs){
     if (is.nil(patient_covs[[cov]])){
       covs_missing <- c(covs_missing, cov)
     }
   }
-  
+
   if (length(covs_missing) == 0) {
     return(TRUE)
   } else if (verbose) {
@@ -36,5 +35,5 @@ check_covs_available <- function(
   } else {
     return(FALSE)
   }
-  
+
 }
