346a07d7864c86e91135cba13a15305e23421748
Kara Woo <kara@insight-rx.com>
2021-05-03 14:31:44 -0700

    Fix style

--- tests/testthat/test_pct_weight_for_age.R
+++ tests/testthat/test_pct_weight_for_age.R
@@ -1,16 +1,16 @@
 test_that("Pct weight for age", {
   expect_equal(
-    pct_weight_for_age(age = 1, weight = 9, sex="male")$percentile, 26.5
+    pct_weight_for_age(age = 1, weight = 9, sex = "male")$percentile, 26.5
   )
   expect_equal(
-    pct_weight_for_age(age = 1, weight = 9, sex="female")$percentile, 51.7
+    pct_weight_for_age(age = 1, weight = 9, sex = "female")$percentile, 51.7
   )
   expect_true(
-    length(names(pct_weight_for_age(age = 1, sex="male"))) > 12
+    length(names(pct_weight_for_age(age = 1, sex = "male"))) > 12
   )
 })
 
 test_that("Pct weight for age errors", {
-  expect_error(pct_weight_for_age(weight = 9, sex="male"))
+  expect_error(pct_weight_for_age(weight = 9, sex = "male"))
   expect_error(pct_weight_for_age())
 })
