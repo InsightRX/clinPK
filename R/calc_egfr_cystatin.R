953452e1f2380054af8acbcdcb4284bf788316de
Ron Keizer <ron@insight-rx.com>
2022-12-22 22:47:35 -0800

    implement hoek method

--- R/calc_egfr_cystatin.R
+++ R/calc_egfr_cystatin.R
@@ -1,6 +1,6 @@
 #' Calculate eGFR based on Cystatin C measurements
 #'
-#' @param method eGFR estimation method, choose from `grubb`, `larsson`
+#' @param method eGFR estimation method, choose from `grubb`, `larsson`, `hoek`
 #' @param cystatin serum cystatin concentration (mg/L)
 #' @param cystatin_unit, only `mg/L` available
 #' @param unit_out eGFR output unit, choose from `ml/min`, `ml/hr`, `l/min`, `l/hr`
@@ -16,9 +16,10 @@ calc_egfr_cystatin <- function (
   unit_out = "mL/min"
 ) {
   available_methods <- c(
-    "grubb", "larsson")
+    "grubb", "larsson", "hoek"
+  )
   available_units <- c(
-    "ml/min", "ml/hr", "l/min", "l/hr"
+    "ml/min", "ml/hr", "l/min", "l/hr", "ml/min/1.73m2"
   )
   method <- gsub("-", "_", tolower(method))
   if(!method %in% available_methods) {
@@ -31,9 +32,21 @@ calc_egfr_cystatin <- function (
   unit <- cystatin_unit
   if(method == "grubb") {
     crcl <- 83.93 * cystatin^(-1.676)
+    if(unit_out == "ml/min/1.73m2") {
+      stop("Output unit not supported for this method.")
+    }
   }
   if(method == "larsson") {
     crcl <- 77.239 * cystatin^(-1.2623)
+    if(unit_out == "ml/min/1.73m2") {
+      stop("Output unit not supported for this method.")
+    }
+  }
+  if(method == "hoek") {
+    crcl <- -4.32 + (80.35/cystatin)
+    if(unit_out != "ml/min/1.73m2") {
+      stop("For Hoek method, the `unit_out` needs to be mL/min/1.73m2.")
+    }
   }
   if (length(grep("^l/min", tolower(unit_out))) > 0) {
     crcl <- crcl / 1000
