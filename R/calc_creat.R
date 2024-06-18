8af76c2063609186a35f7a8f72390899702718da
Jordan Brooks <jordan.brooks@insight-rx.com>
2024-06-18 18:23:54 +0000

    update calc_creat for adults with NHANES regression

--- R/calc_creat.R
+++ R/calc_creat.R
@@ -4,19 +4,27 @@
 #'
 #' Uses equations described in Ceriotti et al. Clin Chem. 2008, and Junge W et al.
 #' Clin Chim Acta. 2004. For age 15-18, a linear interpolation is used between
-#' equations for <15 and >18 years as described in Johanssen A et al. Ther Drug
-#' Monit 2011.
+#' equations for <15 as described in Johanssen A et al. Ther Drug
+#' Monit 2011. Predictions for age >18 are based on an linear regression 
+#' built on open-source data (NHANES). Median height and weight imputed if not 
+#' specified.
+#'  
 #'
 #' @param sex sex, either `male` or `female`
 #' @param age age in years
+#' @param wt weight in kg (default 75 kg)
+#' @param ht height in cm (default 165 cm)
 #' @param digits number of digits to round to
 #' @examples
 #' calc_creat(sex = "male", age = 40)
+#' calc_creat(sex = "male", age = 40, wt = 100, ht = 175)
 #' calc_creat(sex = "male", age = c(10, 17, 60))
 #' @export
 calc_creat <- function (
   sex = NULL,
   age = NULL,
+  wt = NULL,
+  ht = NULL,
   digits = 1
   ) {
   if(is.null(sex) || !all(sex %in% c("male", "female"))) {
@@ -25,14 +33,21 @@ calc_creat <- function (
   if(is.null(age)) {
     stop("Age required.")
   }
-  scr <- rep(0, length(age))
-  scr[age <= 15] <- -2.37330 -(12.91367 * log(age[age <= 15])) + 23.93581 * sqrt(age[age <= 15])
-  sel1 <- (age > 15 & age < 18) & sex == "male"
-  scr[sel1]   <- 9.5471 * age[sel1] - 87.847
-  sel2 <- (age > 15 & age < 18) & sex == "female"
-  scr[sel2] <- 4.7137 * age[sel2] - 15.347
-  scr[age >= 18 & sex == "male"]   <- 84
-  scr[age >= 18 & sex == "female"] <- 69.5
+  if(is.null(wt) & any(age>18)) {
+    warning("75 kg used: recommend specifying wt (kg)")
+  }
+  if(is.null(ht) & any(age>18)) {
+    warning("165 cm used: recommend specifying ht (cm)")
+  }
+    scr <- rep(0, length(age))
+    scr[age <= 15] <- -2.37330 -(12.91367 * log(age[age <= 15])) + 23.93581 * sqrt(age[age <= 15])
+    sel1 <- (age > 15 & age < 18) & sex == "male"
+    scr[sel1]   <- 9.5471 * age[sel1] - 87.847
+    sel2 <- (age > 15 & age < 18) & sex == "female"
+    scr[sel2] <- 4.7137 * age[sel2] - 15.347
+    weight <-  ifelse(is.null(wt),75,wt)
+    height <-ifelse(is.null(ht),165,ht)
+    scr[age>18] <- (-0.1364288 + (0.0041581*age) + (0.1741357*ifelse(sex=="male",1,0)) + (0.0006403*weight) + (0.0039543*height)) * 88.4
   return(list(
     value = round(scr, digits),
     unit = "micromol/L"
--- tests/testthat/test_calc_creat.R
+++ tests/testthat/test_calc_creat.R
@@ -1,10 +1,51 @@
 test_that("calc_create calculates creatinine estimates", {
   expect_equal(
-    calc_creat(
+    suppressWarnings(calc_creat(
       sex = c("male", "female"),
       age = c(20,20)
+    )$value),
+    c(72.6, 57.2)
+  )
+  expect_equal(
+    suppressWarnings( calc_creat(
+      sex = c("male", "female"),
+      age = c(80,80)
+    )$value),
+    c(94.7, 79.3)
+  )
+  expect_equal(
+    suppressWarnings(calc_creat(
+      sex = c("male", "female"),
+      age = c(50,50),
+    )$value),
+    c(83.6, 68.2)
+  )
+  expect_equal(
+    calc_creat(
+      sex = c("male", "female"),
+      age = c(50,50),
+      wt = c(50,50),
+      ht = c(120,120)
     )$value,
-    c(84.0, 69.5)
+    c(66.5, 51.1)
+  )
+  expect_equal(
+    calc_creat(
+      sex = c("male", "female"),
+      age = c(50,50),
+      wt = c(120,120),
+      ht = c(200,200)
+    )$value,
+    c(98.4, 83.0)
+  )
+  expect_equal(
+    calc_creat(
+      sex = c("male", "female"),
+      age = c(50,50),
+      wt = c(50,50),
+      ht = c(120,120)
+    )$value,
+    c(66.5, 51.1)
   )
   expect_equal(
     calc_creat(
