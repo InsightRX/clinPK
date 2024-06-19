bdf5ebc887bdcd6bb3c58583cd1b070ca35eeffa
Jordan Brooks <jordan.brooks@insight-rx.com>
2024-06-19 00:46:43 +0000

    reformated arguments to include adult_method specification

--- R/calc_creat.R
+++ R/calc_creat.R
@@ -2,42 +2,84 @@
 #'
 #' Calculate an estimated serum creatinine. Function takes vectorized input as well.
 #'
-#' Uses equations described in Ceriotti et al. Clin Chem. 2008, and Junge W et al.
-#' Clin Chim Acta. 2004. For age 15-18, a linear interpolation is used between
-#' equations for <15 as described in Johanssen A et al. Ther Drug
-#' Monit 2011. Predictions for age >18 are based on an linear regression 
-#' built on open-source data (NHANES). Median height and weight imputed if not 
-#' specified.
-#'  
+#' For children and adolescents: Uses equations described in Ceriotti et al. 
+#' Clin Chem. 2008, and Junge W et al. Clin Chim Acta. 2004. For age 15-18, 
+#' a linear interpolation is used between equations for <15 Johanssen A et al. 
+#' Ther Drug Monit 2011.
+#' For adults:  Two methods are available Johanssen et al. Ther Drug Monit 2011.
+#' which describes a flat serum creatinine dependent on sex, and Brooks et al 
+#' (unpublished) which is a linear regression built on the NHANES open-source
+#' data and takes into account sex, age, weight, and height. Equation: lm(SCr~Age+Sex+Weight+Height, data=NHANES)
+#'
 #'
 #' @param sex sex, either `male` or `female`
 #' @param age age in years
-#' @param wt weight in kg (default 75 kg)
-#' @param ht height in cm (default 165 cm)
+#' @param weight weight in kg (default 75 kg)
+#' @param height height in cm (default 165 cm)
+#' @param adult_method estimation adult method, `johanssen` (default) or `brooks`,
 #' @param digits number of digits to round to
 #' @examples
 #' calc_creat(sex = "male", age = 40)
-#' calc_creat(sex = "male", age = 40, wt = 100, ht = 175)
+#' calc_creat(sex = "male", age = 40, wt = 100, ht = 175, method="Brooks)
 #' calc_creat(sex = "male", age = c(10, 17, 60))
 #' @export
 calc_creat <- function (
   sex = NULL,
   age = NULL,
-  wt = NULL,
-  ht = NULL,
-  digits = 1
-  ) {
+  weight = NULL,
+  height = NULL,
+  adult_method = c("johanssen", "brooks"),
+  digits = 1 ) {
+  adult_method <- match.arg(adult_method)
+  scr <- switch(
+    adult_method,
+    "johanssen" = calc_creat_johanssen(
+      age = age,
+      sex = sex,
+      adult_method=adult_method
+    ),
+    "brooks" = calc_creat_brooks(
+      age = age,
+      sex = sex,
+      weight = weight,
+      height = height,
+      adult_method=adult_method
+    )
+  )
+  return(list(
+    value = round(scr, digits),
+    unit = "micromol/L",
+    method = tolower(adult_method)
+  ))
+  calc_creat_johanssen <- function(age, sex, adult_method) {
+    if(is.null(sex) || !all(sex %in% c("male", "female"))) {
+      stop("Sex needs to be either male or female!")
+    }
+    if(is.null(age)) {
+      stop("Age required.")
+    }
+    scr <- rep(0, length(age))
+    scr[age <= 15] <- -2.37330 -(12.91367 * log(age[age <= 15])) + 23.93581 * sqrt(age[age <= 15])
+    sel1 <- (age > 15 & age < 18) & sex == "male"
+    scr[sel1]   <- 9.5471 * age[sel1] - 87.847
+    sel2 <- (age > 15 & age < 18) & sex == "female"
+    scr[sel2] <- 4.7137 * age[sel2] - 15.347
+    scr[age >= 18 & sex == "male"]   <- 84
+    scr[age >= 18 & sex == "female"] <- 69.5
+    scr
+  }
+  calc_creat_brooks <- function(age, sex, weight, height, adult_method) {
   if(is.null(sex) || !all(sex %in% c("male", "female"))) {
     stop("Sex needs to be either male or female!")
   }
   if(is.null(age)) {
     stop("Age required.")
   }
-  if(is.null(wt) & any(age>18)) {
-    warning("75 kg used: recommend specifying wt (kg)")
+  if(is.null(weight) && adult_method == "brooks") {
+    stop("Weight is required")
   }
-  if(is.null(ht) & any(age>18)) {
-    warning("165 cm used: recommend specifying ht (cm)")
+  if(is.null(height) && adult_method == "brooks") {
+    stop("Height is required")
   }
     scr <- rep(0, length(age))
     scr[age <= 15] <- -2.37330 -(12.91367 * log(age[age <= 15])) + 23.93581 * sqrt(age[age <= 15])
@@ -45,11 +87,8 @@ calc_creat <- function (
     scr[sel1]   <- 9.5471 * age[sel1] - 87.847
     sel2 <- (age > 15 & age < 18) & sex == "female"
     scr[sel2] <- 4.7137 * age[sel2] - 15.347
-    weight <-  ifelse(is.null(wt),75,wt)
-    height <-ifelse(is.null(ht),165,ht)
     scr[age>18] <- (-0.1364288 + (0.0041581*age) + (0.1741357*ifelse(sex=="male",1,0)) + (0.0006403*weight) + (0.0039543*height)) * 88.4
-  return(list(
-    value = round(scr, digits),
-    unit = "micromol/L"
-  ))
+    scr
+  }
 }
+
--- tests/testthat/test_calc_creat.R
+++ tests/testthat/test_calc_creat.R
@@ -1,64 +1,55 @@
 test_that("calc_create calculates creatinine estimates", {
   expect_equal(
-    suppressWarnings(calc_creat(
+    calc_creat(
       sex = c("male", "female"),
-      age = c(20,20)
-    )$value),
-    c(72.6, 57.2)
+      age = c(14, 14))$value,
+     c(53.1,53.1)
   )
   expect_equal(
-    suppressWarnings( calc_creat(
+    calc_creat(
       sex = c("male", "female"),
-      age = c(80,80)
-    )$value),
-    c(94.7, 79.3)
+      age = c(14, 14))$method,
+    "johanssen"
   )
   expect_equal(
-    suppressWarnings(calc_creat(
+    calc_creat(
+      sex = c("male", "female"),
+      age = c(16, 16))$value,
+    c(64.9,60.1)
+  )
+  expect_equal(
+    calc_creat(
+      sex = c("male", "female"),
+      age = c(16, 40))$method,
+    "johanssen"
+  )
+  expect_equal(
+    calc_creat(
       sex = c("male", "female"),
       age = c(50,50),
-    )$value),
+      weight = c(75,75),
+      height = c(165,165),
+      adult_method="brooks")$value,
     c(83.6, 68.2)
   )
   expect_equal(
     calc_creat(
-      sex = c("male", "female"),
-      age = c(50,50),
-      wt = c(50,50),
-      ht = c(120,120)
-    )$value,
-    c(66.5, 51.1)
+      sex = c("female", "female"),
+      age = c(20,80),
+      weight = c(50,120),
+      height = c(120,200),
+      adult_method="brooks")$value,
+    c(40.1, 94.1)
   )
   expect_equal(
     calc_creat(
-      sex = c("male", "female"),
-      age = c(50,50),
-      wt = c(120,120),
-      ht = c(200,200)
-    )$value,
-    c(98.4, 83.0)
+      sex = c("male", "male"),
+      age = c(20,80),
+      weight = c(50,120),
+      height = c(120,200),
+      adult_method="brooks")$value,
+    c(55.5, 109.4)
   )
-  expect_equal(
-    calc_creat(
-      sex = c("male", "female"),
-      age = c(50,50),
-      wt = c(50,50),
-      ht = c(120,120)
-    )$value,
-    c(66.5, 51.1)
-  )
-  expect_equal(
-    calc_creat(
-      sex = c("male", "female"),
-      age = c(16,16)
-    )$value,
-    c(64.9, 60.1)
-  )
-  expect_equal(
-    calc_creat(
-      sex = c("male", "female"),
-      age = c(16,14)
-    )$value,
-    c(64.9, 53.1)
-  )
-})
+}
+
+  
