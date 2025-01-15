060e62243bfbfe7b390bc61165c0b041412c578e
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2025-01-15 11:03:25 -0800

    revise cefepime neurotoxicity model following author correspondence

--- R/calc_cefepime_neurotoxicity.R
+++ R/calc_cefepime_neurotoxicity.R
@@ -9,23 +9,20 @@
 #' 
 #' @return Probability of cefepime-associated neurotoxicity.
 #' @examples
-#' calc_cefepime_neurotoxicity(trough_concentration = c(12, 16))
+#' calc_cefepime_neurotoxicity(trough_concentration = c(13.7, 17.8))
 #' @export
 calc_cefepime_neurotoxicity <- function(trough_concentration) {
-  # This calculation uses the single predictor cefepime plasma trough
-  # concentration model in Table 2. The coefficient for cefepime plasma trough
-  # concentration is reported as an odds ratio, so we'll first convert it back
-  # to log odds using the inverse transformation.
-  b1 <- log(1.31)
-  
-  # The intercept is not reported in the paper, so we need to solve for it using
-  # some of the information from Fig. 2. Boschung-Pasquier et al. report that the
-  # probability of being neurotoxic is 0.25 at 12 mg/L, and 0.5 at 16 mg/L. Since
-  # these values are rounded, we can solve the intercept for both probability
-  # values, then take their average as the intercept to use for calculations.
-  b0.25 <- log(.25/.75) - b1*12
-  b0.50 <- log(.5/.5) - b1*16
-  b0 <- mean(c(b0.25, b0.50))
-  
+  # The intercept for this model was not reported in the paper, so we contacted
+  # the corresponding author, Dr. Babouee-Flury, for assistance. They provided
+  # us with the unrounded intercept and slope values, along with anonymized
+  # data, which we used to replicate these results. The intercept and slope here
+  # are raw coefficients (log odds). Note that model predictions with these
+  # coefficients do not match what is reported in Figure 2 of the paper---this
+  # is because that curve and the reported values were "produced automatically
+  # in ggplot using a geometric smoother, not fit directly". The true
+  # predictions are actually closer to the probability of being neurotoxic
+  # equalling 0.25 at 13.7mg/L, and 0.50 at 17.8mg/L.
+  b0 <- -4.7777798
+  b1 <- 0.2684747
   stats::plogis(b0 + b1*trough_concentration)
 }
--- tests/testthat/test-calc_cefepime_neurotoxicity.R
+++ tests/testthat/test-calc_cefepime_neurotoxicity.R
@@ -1,4 +1,4 @@
 test_that("cefepime neurotoxicity calculation is correct", {
-  expect_equal(signif(calc_cefepime_neurotoxicity(12), 2), .25)
-  expect_equal(signif(calc_cefepime_neurotoxicity(16), 2), .50)
+  expect_equal(signif(calc_cefepime_neurotoxicity(13.7), 2), .25)
+  expect_equal(signif(calc_cefepime_neurotoxicity(17.8), 2), .50)
 })
